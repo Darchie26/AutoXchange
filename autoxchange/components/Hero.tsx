bfb9fd9586405324946d3d4e651b9ad216c4df28
Deonte Archie <deontearchie@icloud.com>
2023-11-14 09:45:34 -0500

    Created Navbar and Footer component
    ## Work
    - Uploaded the hero image and created Navbar and Footer Component
    
    ## Result
    - The hero banner is almost complete
    - Now I can start creating the navbar and footer

--- autoxchange/components/Hero.tsx
+++ autoxchange/components/Hero.tsx
@@ -25,6 +25,11 @@ const Hero = () => {
                           handleClick={handleScroll}
             />
       </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+            <Image src="/hero.png" alt="Logo Image" fill className= "object-contain"/>
+        </div>
+      </div>
     </div>
   )
 }
